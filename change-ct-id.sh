1e4dafe03e4051c21fddecff2e48ad6998368d08
root <root@pdev.isp.local>
2016-05-05 13:04:36 +0300

    Fast PVE CTID change script added

---
+++ change-ct-id.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Written by demofly
+
+SRCID="$1"
+TGTID="$2"
+DT=`date +'%y.%m.%d at %H:%M:%S'`
+
+TEMPLDIR=`grep "^TEMPLATE" /etc/vz/vz.conf | cut -d= -f2`
+SRCVZVECONF="/etc/vz/conf/${SRCID}.conf"
+test -f "${SRCVZVECONF}" || VZVECONF="/etc/vz/conf/${SRCID}.conf"
+SRCROOT=`grep "^VE_ROOT" "${SRCVZVECONF}" | cut -d= -f2 | cut -d\" -f2 | sed -e "s#\\$VEID#${SRCID}#g"`
+SRCPRIVATE=`grep "^VE_PRIVATE" "${SRCVZVECONF}" | cut -d= -f2 | cut -d\" -f2 | sed -e "s#\\$VEID#${SRCID}#g"`
+
+TGTROOT=`echo "${SRCROOT}" | sed "s#${SRCID}#${TGTID}#"`
+TGTPRIVATE=`echo "${SRCPRIVATE}" | sed "s#${SRCID}#${TGTID}#"`
+
+VARROOT=`echo "${SRCROOT}" | sed "s#${SRCID}#\\$VEID#"`
+VARPRIVATE=`echo "${SRCPRIVATE}" | sed "s#${SRCID}#\\$VEID#"`
+
+TGTVZVECONF=`echo "${SRCVZVECONF}" | sed "s#${SRCID}\.#${TGTID}\.#"`
+
+# DEBUG
+
+#echo "${SRCROOT} -> ${TGTROOT} | ${SRCPRIVATE} -> ${TGTPRIVATE} | ${VARROOT} ${VARPRIVATE}"
+#grep "^VE_ROOT" "${SRCVZVECONF}" | sed -r "s#(^VE_ROOT.*)${SRCROOT}#\1${VARROOT}#"
+#grep "^VE_PRIVATE" "${SRCVZVECONF}" | sed -r "s#(^VE_PRIVATE.*)${SRCPRIVATE}#\1${VARPRIVATE}#"
+#grep "^NETIF" "${SRCVZVECONF}" | sed -r "s#veth${SRCID}\.#veth${TGTID}\.#"
+#exit 0
+
+# Main changes
+
+echo "## Started on ${DT}"
+
+# prepare
+
+ALLOWSTART=`vzctl status ${SRCID} | grep running | wc -l`
+vzctl stop ${SRCID}
+vzquota drop ${SRCID}
+
+# move data dirs
+
+echo "## Moving data dirs:"
+
+mv -v "${SRCROOT}" "${TGTROOT}"
+mv -v "${SRCPRIVATE}" "${TGTPRIVATE}"
+
+echo "## Fork the VE config to ${TGTVZVECONF}:"
+
+cp -v "${SRCVZVECONF}" "${TGTVZVECONF}"
+
+# edit the VE config:
+
+echo "## Editing ${TGTVZVECONF} .."
+
+sed -r "s#(^VE_ROOT.*)${SRCROOT}#\1${VARROOT}#"                 -i "${TGTVZVECONF}"
+sed -r "s#(^VE_PRIVATE.*)${SRCPRIVATE}#\1${VARPRIVATE}#"        -i "${TGTVZVECONF}"
+sed -r "s#veth${SRCID}\.#veth${TGTID}\.#"                       -i "${TGTVZVECONF}"
+
+# Saving the source VE config to ${SRCVZVECONF}.destroy
+
+echo "## Saving the source VE config to ${SRCVZVECONF}.destroy"
+mv -v "${SRCVZVECONF}" "${SRCVZVECONF}.destroy"
+
+# The last accord:
+
+if [[ "${ALLOWSTART}" == "1" ]]
+then
+    echo "## Starting CTID ${TGTID}:"
+    vzctl start ${TGTID}
+fi
+
+echo "## Ended on `date +'%y.%m.%d at %H:%M:%S'`"
